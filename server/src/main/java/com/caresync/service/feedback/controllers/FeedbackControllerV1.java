1a050b91789291c14d585d5bd454ed99eac2f521
IshrakAdit <ishrak.adit07@gmail.com>
2025-07-16 20:02:19 +0600

    feat: update feedback

--- server/src/main/java/com/caresync/service/feedback/controllers/FeedbackControllerV1.java
+++ server/src/main/java/com/caresync/service/feedback/controllers/FeedbackControllerV1.java
@@ -1,6 +1,7 @@
 package com.caresync.service.feedback.controllers;
 
 import com.caresync.service.feedback.dtos.request.FeedbackCreateRequest;
+import com.caresync.service.feedback.dtos.request.FeedbackUpdateRequest;
 import com.caresync.service.feedback.dtos.response.FeedbackResponse;
 import com.caresync.service.feedback.services.abstractions.FeedbackService;
 import jakarta.validation.Valid;
@@ -17,11 +18,6 @@ public class FeedbackControllerV1 {
 
     private final FeedbackService feedbackService;
 
-    @PostMapping
-    public ResponseEntity<FeedbackResponse> addFeedback(@RequestBody @Valid FeedbackCreateRequest request) {
-        return ResponseEntity.ok(feedbackService.addFeedback(request));
-    }
-
     @GetMapping("/doctor/{doctorId}")
     public ResponseEntity<List<FeedbackResponse>> getDoctorFeedbacks(@PathVariable Long doctorId) {
         return ResponseEntity.ok(feedbackService.getFeedbacksForDoctor(doctorId));
@@ -35,6 +31,26 @@ public class FeedbackControllerV1 {
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<FeedbackResponse>> getUserFeedbacks(@PathVariable String userId) {
         return ResponseEntity.ok(feedbackService.getFeedbacksByUser(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<FeedbackResponse> addFeedback(@RequestBody @Valid FeedbackCreateRequest request) {
+        return ResponseEntity.ok(feedbackService.addFeedback(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FeedbackResponse> updateFeedback(@PathVariable Long id,
+            @RequestBody FeedbackUpdateRequest request
+    ) {
+        FeedbackResponse updated = feedbackService.updateFeedback(
+                FeedbackUpdateRequest.builder()
+                        .id(id)
+                        .userId(request.userId())
+                        .rating(request.rating())
+                        .comment(request.comment())
+                        .build()
+        );
+        return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{feedbackId}")
