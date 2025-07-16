79d0c0a64dbbe431be2355dd720f35ab82cd74b1
IshrakAdit <ishrak.adit07@gmail.com>
2025-07-16 18:37:29 +0600

    setup: main file interface implemenation

--- server/src/main/java/com/caresync/service/feedback/FeedbackApplication.java
+++ server/src/main/java/com/caresync/service/feedback/FeedbackApplication.java
@@ -1,13 +1,19 @@
 package com.caresync.service.feedback;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FeedbackApplication {
+public class FeedbackApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeedbackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("CareSync Feedback Service");
 	}
 
 }
